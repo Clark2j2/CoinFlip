de878b41bfd5f9b7f81dbf47ff20a88a6ebedd6d
John Clark <clark2j2@johnmclark.com>
2020-09-22 10:07:59 -0400

    First Commit

---
+++ coinflipping/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './coin'
+
+function App() {
+  return (
+    <div className="App">
+      <fiveHeadsSync />
+    </div>
+  );
+}
+
+export default App;
